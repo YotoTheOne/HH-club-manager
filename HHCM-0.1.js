f573ade4768917d2a6b57a92fe756d99ccde7c24
YotoTheOne <74134758+YotoTheOne@users.noreply.github.com>
2021-04-05 21:28:47 +0200

    Add files via upload

---
+++ HHCM-0.1.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         HaremHeroes Automatic++
+// @namespace    https://github.com/YotoTheOne/HH-club-manager
+// @version      0.1
+// @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
+// @author       YotoTheOne
+// @match        http*://nutaku.haremheroes.com/*
+// @match        http*://*.hentaiheroes.com/*
+// @match        http*://*.gayharem.com/*
+// @grant        GM_addStyle
+// @license      MIT
+// @updateURL   https://github.com/YotoTheOne/HH-club-manager/raw/main/HHCM-0.1.js
+// @downloadURL https://github.com/YotoTheOne/HH-club-manager/raw/main/HHCM-0.1.js
+// ==/UserScript==
+
+// once a day or more, to be determined
+
+
+// go to page "clubs"
+var togoto = $("nav div[rel='content'] a:has(.clubs)").attr("href");
+window.location = window.location.origin + togoto;
+
+// retrieve members list with name, level, girls and contribution
+
+
+// write to file (text, database or calc sheet ?)
